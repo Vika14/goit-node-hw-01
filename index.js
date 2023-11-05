552c26277fe23bfc2ea51605107de859896fc28a
Vika14 <manunya1406@gmail.com>
2023-11-05 18:20:01 +0100

    hw1-2

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
     case "list":
       const allContacts = await contacts.listContacts();
       return console.table(allContacts);
-      break;
+      
     case "get":
       const contactById = await contacts.getContactById(id);
       return console.log(contactById);
-      break;
+      
     case "add":
       const newContact = await contacts.addContact(name, email, phone);
       return console.log(newContact);
-      break;
+      
     case "remove":
       const deletedContact = await contacts.removeContact(id);
       return console.log(deletedContact);
-      break;
+    
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
